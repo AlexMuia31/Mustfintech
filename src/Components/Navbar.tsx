79084e1554f80267e41062e4e4765322967df1be
AlexMuia31 <alexmuia31@gmail.com>
2024-03-27 16:04:16 +0300

    first commit

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar } from "@mui/material";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <AppBar sx={{ background: "transparent" }}>
+      <Toolbar></Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
