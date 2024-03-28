c9135da30eaeec38e3bd16791b2285da9fa09f08
AlexMuia31 <alexmuia31@gmail.com>
2024-03-28 22:22:26 +0300

    table borders

---
+++ src/Components/StyledCheck.ts
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material/styles";
+import Checkbox from "@mui/material/Checkbox";
+
+export const StyledCheck = styled(Checkbox)({
+  "& .MuiSvgIcon-root": {
+    width: "16px",
+    height: "16px",
+    borderRadius: "4px",
+  },
+});
--- src/Components/Tables.tsx
+++ src/Components/Tables.tsx
@@ -7,6 +7,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { StyledCheck } from "./StyledCheck";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -34,39 +35,47 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 function createData(
+  checkbox: JSX.Element,
   name: string,
   calories: number,
   fat: number,
   carbs: number,
   protein: number
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { checkbox, name, calories, fat, carbs, protein };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData(<StyledCheck />, "Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData(<StyledCheck />, "Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData(<StyledCheck />, "Eclair", 262, 16.0, 24, 6.0),
+  createData(<StyledCheck />, "Cupcake", 305, 3.7, 67, 4.3),
+  createData(<StyledCheck />, "Gingerbread", 356, 16.0, 49, 3.9),
 ];
 
 export default function Tables() {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 1170, width: "100%" }}>
+      <Table sx={{ minWidth: 1170 }}>
         <TableHead>
           <TableRow>
-            <StyledTableCell align="center">Dessert</StyledTableCell>
-            <StyledTableCell align="center">Calories</StyledTableCell>
-            <StyledTableCell align="center">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="center">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="center">Protein&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="center">
+              <StyledCheck />
+            </StyledTableCell>
+            <StyledTableCell align="center">NO</StyledTableCell>
+            <StyledTableCell align="center">기존유형</StyledTableCell>
+            <StyledTableCell align="center">신청유형</StyledTableCell>
+            <StyledTableCell align="center">제출서류</StyledTableCell>
+            <StyledTableCell align="center">신청일시</StyledTableCell>
+            <StyledTableCell align="center">승인여부</StyledTableCell>
+            <StyledTableCell align="center">승인거부 사유</StyledTableCell>
+            <StyledTableCell align="center">승인거부 사유</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <StyledTableRow key={row.name}>
+              <StyledTableCell align="center">{row.checkbox}</StyledTableCell>
               <StyledTableCell align="center">{row.name}</StyledTableCell>
               <StyledTableCell align="center">{row.calories}</StyledTableCell>
               <StyledTableCell align="center">{row.fat}</StyledTableCell>
