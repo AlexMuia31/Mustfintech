1073bcb9f3d47c540d74d795026a696701fdbc7a
AlexMuia31 <alexmuia31@gmail.com>
2024-03-30 11:34:55 +0300

    table data done

---
+++ src/Components/CustomChips.ts
@@ -0,0 +1,17 @@
+import { styled } from "@mui/material/styles";
+import { Chip } from "@mui/material";
+
+export const YellowChip = styled(Chip)({
+  background: "#FFEDD5",
+  color: "#9A3412",
+});
+
+export const RedChip = styled(Chip)({
+  backgroundColor: "#FEE2E2",
+  color: "#991B1B",
+});
+
+export const GreenChip = styled(Chip)({
+  backgroundColor: "#DCFCE7",
+  color: "#166534",
+});
